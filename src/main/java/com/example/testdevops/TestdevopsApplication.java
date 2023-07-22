5776d8588886d69cc0ad37ef60465ead72590d7d
wang gang <wg@wozaijia.com>
2023-07-22 21:00:40 +0800

    first commit

---
+++ src/main/java/com/example/testdevops/TestdevopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.testdevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestdevopsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestdevopsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/testdevops/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.example.testdevops.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class TestController {
+
+    @GetMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+}
